8bf992c5680f1ab637f08b99d3e4576d99bb1e8a
Puja <puja@giantswarm.io>
2016-05-18 12:17:10 +0200

    changed to lean demo api endpoint

--- leanix/api-calls.sh
+++ leanix/api-calls.sh
@@ -2,9 +2,9 @@
 curl -X POST \
 -H "Content-Type:application/json" \
 -d @service.json \
-http://example.com/path/to/resource
+https://app.leanix.net/demo/api/v1
 
 curl -X POST \
 -H "Content-Type:application/json" \
 -d @component.json \
-http://example.com/path/to/resource
+https://app.leanix.net/demo/api/v1
